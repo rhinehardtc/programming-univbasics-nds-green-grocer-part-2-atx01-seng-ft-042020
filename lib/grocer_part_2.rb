b2819c82c23624a0c354ff9dc57780bc7a05a488
Cameron Rhinehardt <rhinehardtc@gmail.com>
2020-04-17 02:46:24 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -2,36 +2,8 @@ require_relative './part_1_solution.rb'
 require 'pry'
 
 def apply_coupons(cart, coupons)
-  
-  coupon_items = []
   coupons.each do |object|
-    coupon_items << object[:item]
-  end
-  pp coupon_items
-  #-----------------------------------------------#
-  cart.each do |object|
-    if coupon_items.include?(object[:item]) == true
-      new_object = object
-      new_object[:item] = new_object[:item] + " W/COUPON"
-      
-    end
-    pp new_object
-  end
-  #-----------------------------------------------#
-  i = 0
-  while i < cart.length do
-    ci = 0
-    while ci < coupons.length do
-      
-      if cart[i][:item].include?(coupons[ci][:item]) == true
-        cart[i][:count] = coupons[ci][:num]
-        cart[i][:price] = (coupons[ci][:cost] / coupons[ci][:num]).round(2)
-      end
-      ci += 1
-    end
-    i += 1
-  end
-  cart
+  
 end
 
 ##
